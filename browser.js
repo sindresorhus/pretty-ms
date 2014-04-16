ee2e210af4d67a4b99de9f9410b8fe1923e0555d
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-16 22:47:09 +0200

    simplify

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.prettyMs=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"Focm2+":[function(_dereq_,module,exports){
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.prettyMs=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
 var parseMs = _dereq_('parse-ms');
 
@@ -38,9 +38,7 @@ module.exports = function (ms, opts) {
 	return ret.join(' ');
 };
 
-},{"parse-ms":3}],"pretty-ms":[function(_dereq_,module,exports){
-module.exports=_dereq_('Focm2+');
-},{}],3:[function(_dereq_,module,exports){
+},{"parse-ms":2}],2:[function(_dereq_,module,exports){
 'use strict';
 module.exports = function (ms) {
 	if (typeof ms !== 'number') {
@@ -56,6 +54,6 @@ module.exports = function (ms) {
 	};
 };
 
-},{}]},{},["Focm2+"])
-("Focm2+")
+},{}]},{},[1])
+(1)
 });
--- readme.md
+++ readme.md
@@ -5,8 +5,6 @@
 
 ## Install
 
-Download [manually](https://github.com/sindresorhus/pretty-ms/releases) or with a package-manager.
-
 ```bash
 $ npm install --save pretty-ms
 ```
@@ -23,8 +21,6 @@ $ component install sindresorhus/pretty-ms
 ## Usage
 
 ```js
-var prettyMs = require('pretty-ms');
-
 prettyMs(1337000000);
 //=> '15d 11h 23m 20s'
 
@@ -42,8 +38,6 @@ prettyMs(1337, {compact: true});
 prettyMs(new Date(2014, 0, 1, 10, 40) - new Date(2014, 0, 1, 10, 5))
 //=> '35m'
 ```
-
-Can be used with require, global and AMD in the browser.
 
 
 ## API
@@ -64,13 +58,9 @@ Only show the first unit: `1h 10m` ➔ `~1h`.
 
 ## CLI
 
-You can also use it as a CLI app by installing it globally:
-
 ```bash
 $ npm install --global pretty-ms
 ```
-
-### Usage
 
 ```bash
 $ pretty-ms --help
