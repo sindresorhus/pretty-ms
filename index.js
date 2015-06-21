57133765d8f005737c21940732aaeb887e096425
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-21 12:05:47 +0530

    Start using the npm module: plur
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.js
+++ index.js
@@ -15,30 +15,19 @@ module.exports = function (ms, opts) {
 	}
 
 	opts = opts || {};
+	var plur = require('plur');
 
 	if (ms < 1000) {
-		if (opts.verbose && Math.ceil(ms) === 1.0) {
-			return Math.ceil(ms) + ' millisecond';
-		}
-		if (opts.verbose) {
-			return Math.ceil(ms) + ' milliseconds';
-		}
-		return Math.ceil(ms) + 'ms';
+		return Math.ceil(ms) + (opts.verbose ? plur(' millisecond', Math.ceil(ms)) : 'ms');
 	}
 
 	var secDecimalDigits = typeof opts.secDecimalDigits === 'number' ? opts.secDecimalDigits : 1;
 	var ret = [];
 	var parsed = parseMs(ms);
 
-	if (opts.verbose) {
-		ret = add(ret, parsed.days, parsed.days === 1 ? ' day' : ' days');
-		ret = add(ret, parsed.hours, parsed.hours === 1 ? ' hour' : ' hours');
-		ret = add(ret, parsed.minutes, parsed.minutes === 1 ? ' minute' : ' minutes');
-	} else {
-		ret = add(ret, parsed.days, 'd');
-		ret = add(ret, parsed.hours, 'h');
-		ret = add(ret, parsed.minutes, 'm');
-	}
+	ret = add(ret, parsed.days, (opts.verbose ? plur(' day', parsed.days) : 'd'));
+	ret = add(ret, parsed.hours, (opts.verbose ? plur(' hour', parsed.hours) : 'h'));
+	ret = add(ret, parsed.minutes, (opts.verbose ? plur(' minute', parsed.minutes) : 'm'));
 
 	if (opts.compact) {
 		if (opts.verbose) {
