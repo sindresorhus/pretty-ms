6adf3cdaf8f2ddf55dff8889313ef8a8338b66db
Henry <mail@henrygressmann.de>
2019-10-03 23:37:15 +0200

    fix formatting
    Signed-off-by: Henry <mail@henrygressmann.de>

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = (milliseconds, options = {}) => {
 
 	if (milliseconds >= (1000 * 60) - 50 && !options.separateMilliseconds &&
 		!options.formatSubMilliseconds) {
-		const difference = 60 - milliseconds % 60
+		const difference = 60 - (milliseconds % 60);
 		if (difference <= 50) {
 			milliseconds += difference;
 		}
