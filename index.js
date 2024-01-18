286fe0d5c1e2d3e9a0a5f4f1cde3b05d315946be
fisker <lionkay@gmail.com>
2024-01-18 17:19:02 +0800

    Minor tweak

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ONE_DAY_IN_MILLISECONDS = 24n * 60n * 60n * 1000n;
 
 export default function prettyMilliseconds(milliseconds, options = {}) {
 	const isBigInt = typeof milliseconds === 'bigint';
-	if (!Number.isFinite(milliseconds) && !isBigInt) {
+	if (!isBigInt && !Number.isFinite(milliseconds)) {
 		throw new TypeError('Expected a finite number or bigint');
 	}
 
