6b6ccb3d454a30f635eb21fe728fc235f1738c8f
fisker <lionkay@gmail.com>
2024-01-18 17:05:53 +0800

    Update docs

--- index.test-d.ts
+++ index.test-d.ts
@@ -21,3 +21,24 @@ expectType<string>(
 expectType<string>(
 	prettyMilliseconds(1_335_669_000, {colonNotation: true}),
 );
+
+expectType<string>(prettyMilliseconds(1_335_669_000n));
+expectType<string>(prettyMilliseconds(1_335_669_000n, {secondsDecimalDigits: 1}));
+expectType<string>(
+	prettyMilliseconds(1_335_669_000n, {millisecondsDecimalDigits: 2}),
+);
+expectType<string>(
+	prettyMilliseconds(1_335_669_000n, {keepDecimalsOnWholeSeconds: true}),
+);
+expectType<string>(prettyMilliseconds(1337n, {compact: true}));
+expectType<string>(prettyMilliseconds(1_335_669_000n, {unitCount: 2}));
+expectType<string>(prettyMilliseconds(1_335_669_000n, {verbose: true}));
+expectType<string>(
+	prettyMilliseconds(1_335_669_000n, {separateMilliseconds: true}),
+);
+expectType<string>(
+	prettyMilliseconds(1_335_669_000n, {formatSubMilliseconds: true}),
+);
+expectType<string>(
+	prettyMilliseconds(1_335_669_000n, {colonNotation: true}),
+);
